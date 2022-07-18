0b04f5da9065d73251ed5e07acad9eba9755990c
Jake <jake.ord345@gmail.com>
2022-07-18 14:07:55 +0100

    setup auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,25 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+
 export default function Index() {
+  const { data: session } = useSession();
+
   return (
     <main className='text-2xl'>
       <h1>Garchomper - an image sharer </h1>
+
+      {!session && (
+        <section>
+          <p>Please Sign In</p>
+          <button onClick={() => signIn("google")}>Sign In</button>
+        </section>
+      )}
+
+      {session && (
+        <section>
+          <p>Hello, {session.user?.name}</p>
+          <button onClick={() => signOut()}>Sign Out</button>
+        </section>
+      )}
     </main>
   );
 }
